530c9c91472945a6259d41336b84fd6909ccf0cc
samuelhg <samuelhg@163.com>
2025-06-18 00:29:27 +0800

    fix theme define

--- source/conf.py
+++ source/conf.py
@@ -5,6 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_rtd_theme
 
 project = 'demo'
 copyright = '2025, Samuelhg'
